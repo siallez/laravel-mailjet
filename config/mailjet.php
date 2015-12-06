7d6184afed0a0cc918ad04b4106c18874e9561c4
siallez <aankering@hotmail.com>
2015-12-06 17:15:38 +0100

    First commit

---
+++ config/mailjet.php
@@ -0,0 +1,6 @@
+<?php 
+
+return array(
+	'apikey_public' => env('MAILJET_APIKEY_PUBLIC'),
+	'apikey_private' => env('MAILJET_APIKEY_PRIVATE')
+);
---
+++ src/MailjetServiceProvider.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Siallez\Mailjet;
+
+use Illuminate\Support\ServiceProvider;
+
+class MailjetServiceProvider extends ServiceProvider
+{
+	/**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+	protected $defer = true;
+	
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+		$this->mergeConfigFrom(__DIR__ . '/../config/mailjet.php', 'mailjet');
+		
+		$this->app->bind('\Mailjet\Client', function ($app) {
+			return new \Mailjet\Client($app['config']['mailjet.apikey_public'], $app['config']['mailjet.apikey_private']);
+		});
+    }
+	
+	/**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['\Mailjet\Client'];
+    }
+}
